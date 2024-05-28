03964b54dcf5866cb745ae28faf99333f23a0d04
Bryan Karta <karta.bryan@gmail.com>
2024-05-28 22:10:22 +0700

    module-02:04-01

---
+++ Module-2/04/tailwind/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="container mx-auto text-center p-2">
+      <div className="border-2 border-solid rounded-md border-black">
+        <p className="text-4xl text-red-500">Ini Tailwind CSS</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
